e72cdae83ac943d344bb58676bd88d437c6b0788
Harry Sutcliffe <hsutcliffe@berkeley.edu>
2017-09-11 00:32:04 -0400

    basic implementation

---
+++ src/com/company/Main.java
@@ -0,0 +1,37 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    //TODO: add functionality that asks for encode or decode
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        while (true) {
+            System.out.println("Enter a string: ");
+            String input = reader.nextLine();
+            System.out.println(encode(input));
+            System.out.println();
+        }
+
+    }
+
+    public static String encode(String input) {
+        StringBuilder encodedString = new StringBuilder();
+        for (int i = 0; i < input.length(); i += 1) {
+            char c = input.charAt(i);
+            if (c >= 'a' && c <= 'm') {
+                c += 13;
+            } else if (c >= 'A' && c <= 'M') {
+                c += 13;
+            } else if (c >= 'n' && c <= 'z') {
+                c -= 13;
+            } else if (c >= 'N' && c <= 'Z') {
+                c -= 13;
+            }
+            encodedString.append(c);
+        }
+        return encodedString.toString();
+    }
+}
